9b080f7f5016bddb0d45791dd8718c65ae77ce5f
Karim Farkhutdinov <KF.Farkhutdinov@yandex.ru>
2023-08-14 22:43:54 +0300

    Добавил препроцессоры в проект

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,93 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import gulpIf from "gulp-if";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import postcss from "gulp-postcss";
+import postUrl from "postcss-url";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import squoosh from "gulp-libsquoosh";
+import svgo from "gulp-svgmin";
+import { stacksvg } from "gulp-stacksvg";
+import { deleteAsync } from "del";
+import browser from "browser-sync";
+import bemlinter from "gulp-html-bemlinter";
+import { htmlValidator } from "gulp-w3c-html-validator";
 
-// Styles
+const sass = gulpSass(dartSass);
+let isDevelopment = true;
 
-export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+export function processMarkup() {
+  return gulp.src("src/*.html").pipe(gulp.dest("build"));
+}
+
+export function lintBem() {
+  return gulp.src("src/*.html").pipe(bemlinter());
+}
+
+export function validateMarkup() {
+  return gulp
+    .src("src/*.html")
+    .pipe(htmlValidator.analyzer())
+    .pipe(htmlValidator.reporter({ throwErrors: true }));
+}
+
+export function processStyles() {
+  return gulp
+    .src("src/sass/*.scss", { sourcemaps: isDevelopment })
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([postUrl({ assetsPath: "../" }), autoprefixer(), csso()]))
+    .pipe(gulp.dest("build/css", { sourcemaps: isDevelopment }))
     .pipe(browser.stream());
 }
 
-// Server
+export function optimizeImages() {
+  return gulp
+    .src("src/img/**/*.{png,jpg}")
+    .pipe(gulpIf(!isDevelopment, squoosh()))
+    .pipe(gulp.dest("build/img"));
+}
 
-const server = (done) => {
+export function createWebp() {
+  return gulp
+    .src("src/img/**/*.{png,jpg}")
+    .pipe(
+      squoosh({
+        webp: {},
+      })
+    )
+    .pipe(gulp.dest("build/img"));
+}
+
+export function optimizeVector() {
+  return gulp
+    .src(["src/img/**/*.svg", "!src/img/icons/**/*.svg"])
+    .pipe(svgo())
+    .pipe(gulp.dest("build/img"));
+}
+
+export function createStack() {
+  return gulp
+    .src("src/img/icons/**/*.svg")
+    .pipe(svgo())
+    .pipe(stacksvg())
+    .pipe(gulp.dest("build/img/icons"));
+}
+
+export function copyAssets() {
+  return gulp
+    .src(["src/fonts/**/*.{woff2,woff}", "src/*.ico", "src/*.webmanifest"], {
+      base: "src",
+    })
+    .pipe(gulp.dest("build"));
+}
+
+export function startServer(done) {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build",
     },
     cors: true,
     notify: false,
@@ -32,14 +96,37 @@ const server = (done) => {
   done();
 }
 
-// Watcher
-
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+function reloadServer(done) {
+  browser.reload();
+  done();
 }
 
+function watchFiles() {
+  gulp.watch("src/sass/**/*.scss", gulp.series(processStyles));
+  gulp.watch("src/*.html", gulp.series(processMarkup, reloadServer));
+}
 
-export default gulp.series(
-  styles, server, watcher
-);
+function compileProject(done) {
+  gulp.parallel(
+    processMarkup,
+    processStyles,
+    optimizeVector,
+    createStack,
+    copyAssets,
+    optimizeImages,
+    createWebp
+  )(done);
+}
+
+function deleteBuild() {
+  return deleteAsync("build");
+}
+
+export function buildProd(done) {
+  isDevelopment = false;
+  gulp.series(deleteBuild, compileProject)(done);
+}
+
+export function runDev(done) {
+  gulp.series(deleteBuild, compileProject, startServer, watchFiles)(done);
+}
