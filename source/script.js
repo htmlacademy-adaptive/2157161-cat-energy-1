722b41dc36b0eb7dfbea50aa18e266256f07a51e
Karim Farkhutdinov <KF.Farkhutdinov@yandex.ru>
2023-12-16 01:26:50 +0500

    добавил в living-example анимацию кота до и после в мобильном разрешении

--- source/script.js
+++ source/script.js
@@ -1,6 +1,9 @@
 let navMain = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".main-nav__toggle");
 
+const sliderListElement = document.querySelector(".cat-result__list");
+const sliderControlElement = document.querySelector(".cat-result__control");
+
 navMain.classList.remove("main-nav--nojs");
 
 navToggle.addEventListener("click", function () {
@@ -12,3 +15,16 @@ navToggle.addEventListener("click", function () {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+if (sliderControlElement) {
+  let flag = false;
+  sliderControlElement.addEventListener("click", function () {
+    if (flag) {
+      sliderListElement.classList.toggle("cat-result__list--after");
+      sliderListElement.classList.toggle("cat-result__list--before");
+    } else {
+      sliderListElement.classList.add("cat-result__list--after");
+      flag = true;
+    }
+  });
+}
